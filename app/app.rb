7c73c8b3d592c1acb72de2e9ad20b550dd4b9245
Thomas Ikimi <themerc01@gmail.com>
2017-09-20 15:16:29 +0100

    deploying to heroku

--- app/app.rb
+++ app/app.rb
@@ -1,3 +1,5 @@
+ENV["RACK_ENV"] ||= "development"
+
 require 'sinatra/base'
 
 require_relative 'models/link'
@@ -17,4 +19,7 @@ class BookmarkManager < Sinatra::Base
     Link.create(url: params[:url], title: params[:title])
     redirect '/links'
   end
+
+  run! if app_file == $0
+
 end
